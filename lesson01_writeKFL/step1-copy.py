ebfc5d058d79d6364342f4fa141830cf4ebe191b
Bulgakoff <hlbu@yandex.ru>
2019-11-30 22:32:44 +0300

    исправил в мастере

--- lesson01_writeKFL/step1-copy.py
+++ lesson01_writeKFL/step1-copy.py
@@ -15,27 +15,28 @@ def create_folder(name):
         print('МОжно двигаться дальше ', d)
 
 
-def info_path():
+def info_path(fol_only=False):
     result = os.listdir()
     # result1 = os.listdir()
-
-    result = [f for f in result if os.path.isdir(f)]
+    if fol_only:
+        result = [f for f in result if os.path.isdir(f)]
 
     print(f'=====>{result}')
     # print(result1)
-def del_ff(name):
-    if os.path.isdir(name):
-        os.rmdir(name)
-    else:
-        os.remove(name)
+# def del_ff(name):
+#     if os.path.isdir(name):
+#         os.rmdir(name)
+#     else:
+#         os.remove(name)
 
 
 
 
 # что бы в другом модуле не запускался код
 if __name__ == '__main__':
-    create_file('text.js')
+    create_file('text.txt', 'eeeee')
+
     create_file('text.js', 'some text')
     create_folder('new_folder2')
-    del_ff('text.js')
-    info_path()
+    # del_ff('text.js')
+    info_path(False)
