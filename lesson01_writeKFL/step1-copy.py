87aa3667d28c270c07fea00ba07286806552be6a
Bulgakoff <hlbu@yandex.ru>
2019-11-30 22:17:09 +0300

    Начало по kfm16

--- lesson01_writeKFL/step1-copy.py
+++ lesson01_writeKFL/step1-copy.py
@@ -1,23 +1,34 @@
 # функция для создания файла
 import os
+
+
 def create_file(name, text=None):
     with open(name, 'w', encoding='utf-8') as f:
         if text:
             f.write(text)
+
 
 def create_folder(name):
     try:
         os.mkdir(name)
     except FileExistsError as d:
         print('МОжно двигаться дальше ', d)
-def info_path(files_only = False,fo_only=False):
-    result = os.listdir()
-    result1 = os.listdir()
 
-    result =[f for f in result if os.path.isfile(f)]
-    result1 =[fo for fo in result1 if os.path.isdir(fo)]
-    print(result)
-    print(result1)
+
+def info_path():
+    result = os.listdir()
+    # result1 = os.listdir()
+
+    result = [f for f in result if os.path.isdir(f)]
+
+    print(f'=====>{result}')
+    # print(result1)
+def del_ff(name):
+    if os.path.isdir(name):
+        os.rmdir(name)
+    else:
+        os.remove(name)
+
 
 
 
@@ -26,7 +37,5 @@ if __name__ == '__main__':
     create_file('text.js')
     create_file('text.js', 'some text')
     create_folder('new_folder2')
-    info_path(True)
-
-
-
+    del_ff('text.js')
+    info_path()
